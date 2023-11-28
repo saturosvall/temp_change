ebe17dcc7c03e56b70f26b648dc6f1bfeb6f11db
Satu Rosvall <satu.rosvall@esedulainen.fi>
2023-11-28 09:48:51 +0200

    Ehtolauseet ja desimaalin valinta

--- main.js
+++ main.js
@@ -1,46 +1,65 @@
-
-console.log("testi")
 document.getElementById('formMuunna').addEventListener('submit', muunnaLampo);
-
+document.body.style.fontFamily = "Tahoma";
 
 function muunnaLampo(event) {
     event.preventDefault()
-    // muuttujien määrittely
     
-
-    // valikosta valittava muunnos
-    let valinta = document.getElementById("yksikot");
-    valinta = valinta.options[valinta.selectedIndex].text;
+    // valikosta valittava syote
+    let valinta = document.getElementById("yksikot").value;
     
     // muutettava lämpötila
     let syote = document.querySelector('input').value;
-    //let syote = Number(document.getElementById('flampo').value);
     
+    // desimaalin valinta
+    let desimaali = '';
 
+    if(document.getElementById('yksi').checked == true) {
+        desimaali = "yksi";
+    } else if(document.getElementById('kaksi').checked == true) {
+        desimaali = 'kaksi';
+    } else {
+        desimaali = 'kolme';
+    }
+
+   
     // laskutoimitukset
     
-    if(valinta == "cf") 
-    syote = (syote)* 1.8 + 32; // kaava celsiuksesta fahrenheitiin // kaava celsiuksesta fahrenheitiin
+    if(valinta == "cf") {
+        syote = (syote)* 1.8 + 32; // kaava celsiuksesta fahrenheitiin
+    }
+    else if(valinta == "ck") {
+        syote = parseInt(syote) + 273.15; // kaava celsiuksesta kelviniin
+    }
+    else if(valinta == "fc") {
+        syote = (syote - 32) / 1.8; // kaava fahrenheitista celsiukseen
+    }
+    else if(valinta == "fk") {
+        syote = parseInt(syote) + 459.67 / 1.8; // kaava fahrenheitista kelviniin
+    }
+    else if(valinta == "kf") {
+        syote = syote * 1.8 -459.67; // kaava kelvinistä fahrenheitiin
+    }
+    else if(valinta == "kc") {
+        syote = syote - 273.15; // kaava kelvininstä celsiukseen
+    }
+    else if(valinta =="info") {
+        syote = "Valitse muunnin";
+    }
+    else {
+        syote = "Syötä lämpötila";
+    }
     
-    else if(valinta == "ck") 
-    syote = syote + 273,15;// kaava celsiuksesta kelviniin
-    
-    else if(valinta == "fc") 
-    syote = (syote - 32) / 1.8// kaava fahrenheitista celsiukseen
-    
-    else if(valinta == "fk") 
-    syote = (syote + 459.67) / 1.8 // kaava fahrenheitista kelviniin
-    
-    else if(valinta == "kf") 
-    syote = syote * 1.8 -459.67 // kaava kelvinistä fahrenheitiin
-    
-    else if (valinta == "kc") 
-    syote = syote - 273,15 // kaava kelvininstä celsiukseen +
+    switch (desimaali) {
+        case 'yksi':
+            document.getElementById('tulos').innerHTML = syote.toFixed(1);
+            break;
+        case 'kaksi':
+            document.getElementById('tulos').innerHTML = syote.toFixed(2);
+            break;
+        default:
+            document.getElementById('tulos').innerHTML = syote.toFixed(3);
+        }
 
-    else (valinta == "")
-    syote = syote
-    
-    console.log(syote)
-    console.log(valinta)
-    document.getElementById('tulos').innerHTML = syote;
+    document.body.style.backgroundColor = "#dedede";
 }
+
