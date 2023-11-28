1a4b9c37c3517fe679bd80ecb84730ffd19e7fdb
Satu Rosvall <satu.rosvall@esedulainen.fi>
2023-11-28 11:24:16 +0200

    HTML ulkoasu ja huomiokentän lisääminen

--- main.js
+++ main.js
@@ -9,8 +9,13 @@ function muunnaLampo(event) {
     
     // muutettava lämpötila
     let syote = document.querySelector('input').value;
+
+    // syötteiden validointi
+    if (isNaN(syote) || syote=='') {
+        alert("Syötä numero!")
+    }
     
-    // desimaalin valinta
+    // desimaalin todentaminen
     let desimaali = '';
 
     if(document.getElementById('yksi').checked == true) {
@@ -25,39 +30,59 @@ function muunnaLampo(event) {
     // laskutoimitukset
     
     if(valinta == "cf") {
-        syote = (syote)* 1.8 + 32; // kaava celsiuksesta fahrenheitiin
+        tulos = (syote)* 1.8 + 32; // kaava celsiuksesta fahrenheitiin
+        document.getElementById('tulos').innerHTML = "Fahrenheitia";
+
+        if(tulos < -459.40) {
+            document.getElementById('huomio').innerHTML = "Huom! Lämpötila on pienempi kuin absoluuttinen nollapiste!";
+        }
     }
     else if(valinta == "ck") {
-        syote = parseInt(syote) + 273.15; // kaava celsiuksesta kelviniin
+        tulos = parseInt(syote) + 273.15; // kaava celsiuksesta kelviniin
+        if(tulos < 0.15) {
+            document.getElementById('huomio').innerHTML = "Huom! Lämpötila on pienempi kuin absoluuttinen nollapiste!";
+        }
     }
     else if(valinta == "fc") {
-        syote = (syote - 32) / 1.8; // kaava fahrenheitista celsiukseen
+        tulos = (syote - 32) / 1.8; // kaava fahrenheitista celsiukseen
+        if(tulos < -273.15) {
+            document.getElementById('huomio').innerHTML = "Huom! Lämpötila on pienempi kuin absoluuttinen nollapiste!";
+        }
     }
     else if(valinta == "fk") {
-        syote = parseInt(syote) + 459.67 / 1.8; // kaava fahrenheitista kelviniin
+        tulos = parseInt(syote) + 459.67 / 1.8; // kaava fahrenheitista kelviniin
+        if(tulos < 0) {
+            document.getElementById('huomio').innerHTML = "Huom! Lämpötila on pienempi kuin absoluuttinen nollapiste!";
+        }
     }
     else if(valinta == "kf") {
-        syote = syote * 1.8 -459.67; // kaava kelvinistä fahrenheitiin
+        tulos = syote * 1.8 -459.67; // kaava kelvinistä fahrenheitiin
+        if(tulos < -459.40) {
+            document.getElementById('huomio').innerHTML = "Huom! Lämpötila on pienempi kuin absoluuttinen nollapiste!";
+        }
     }
     else if(valinta == "kc") {
-        syote = syote - 273.15; // kaava kelvininstä celsiukseen
+        tulos = syote - 273.15; // kaava kelvininstä celsiukseen
+        if(tulos < -273.15) {
+            document.getElementById('huomio').innerHTML = "Huom! Lämpötila on pienempi kuin absoluuttinen nollapiste!";
+        }
     }
     else if(valinta =="info") {
-        syote = "Valitse muunnin";
-    }
-    else {
-        syote = "Syötä lämpötila";
+        document.getElementById('tulos').innerHTML = "Valitse muunnin!";
+    } else {
+        document.getElementById('tulos').innerHTML = "Valitse muunnin!";
     }
     
+    // tulos desimaalin mukaan
     switch (desimaali) {
         case 'yksi':
-            document.getElementById('tulos').innerHTML = syote.toFixed(1);
+            document.getElementById('tulos').innerHTML = tulos.toFixed(1);
             break;
         case 'kaksi':
-            document.getElementById('tulos').innerHTML = syote.toFixed(2);
+            document.getElementById('tulos').innerHTML = tulos.toFixed(2);
             break;
         default:
-            document.getElementById('tulos').innerHTML = syote.toFixed(3);
+            document.getElementById('tulos').innerHTML = tulos.toFixed(3);
         }
 
     document.body.style.backgroundColor = "#dedede";
